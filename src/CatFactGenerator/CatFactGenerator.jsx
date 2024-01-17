82d0a036a1db3579e28fa291585a53f79f0f9e64
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2024-01-17 22:23:37 +0545

    Added conditional rendering of cat facts

--- src/CatFactGenerator/CatFactGenerator.jsx
+++ src/CatFactGenerator/CatFactGenerator.jsx
@@ -3,18 +3,22 @@ import Axios from "axios";
 
 const CatFactGenerator = () => {
   const [catFact, setCatFact] = useState("");
+  const [count, setCount] = useState(0);
 
   const fetchCatFact = () => {
     Axios.get("https://catfact.ninja/fact").then((res) => {
       setCatFact(res.data.fact);
     });
+    setCount((prev) => prev + 1);
   };
 
   return (
     <div>
       <div>
         <h1 className="h1">Cat Fact Generator</h1>
-        <p className="cat-paragraph">{catFact}</p>
+        <p className="cat-paragraph">
+          {count === 0 ? "Click on Get Fact to see facts" : catFact}
+        </p>
         <button onClick={fetchCatFact} className="btn">
           Get Fact
         </button>
